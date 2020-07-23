4db895ccaba34c421a45ee4a03360c5a14d4823d
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-07-23 21:57:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,15 @@ provider "ibm" {}
 
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id = "bs79g2k20piq1r143ps0"
-  admin=true
+}
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = data.ibm_container_cluster_config.cluster_config.host
+  token                  = data.ibm_container_cluster_config.cluster_config.token
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "terraform-example-namespace"
+  }
 }
