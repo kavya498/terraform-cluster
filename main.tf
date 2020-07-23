ae716734a1b8096489f44d2ca89766d28421d56b
hkantare <hkantare@in.ibm.com>
2020-07-23 16:41:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "ibm" {}
-resource "ibm_container_cluster" "cluster" {
+/*resource "ibm_container_cluster" "cluster" {
   name              = "${var.cluster_name}${random_id.name.hex}"
   datacenter        = "${var.datacenter}"
   no_subnet         = true
@@ -8,11 +8,11 @@ resource "ibm_container_cluster" "cluster" {
   hardware         = "shared"
   public_vlan_id    = "${var.public_vlan_id}"
   private_vlan_id   = "${var.private_vlan_id}"
-}
+}*/
 
 
 data "ibm_container_cluster_config" "cluster_config" {
-  cluster_name_id = "${ibm_container_cluster.cluster.id}"
+  cluster_name_id = "c88557ca-c79e-4cf0-88b1-1713cdd6976d"
 }
 
 resource "random_id" "name" {
